8131b231783f0e6820d82ae7383e20ee60ecf646
Hari21-Tech <sri21harisesh@gmail.com>
2025-07-10 19:58:53 +0530

    config

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from contextlib import asynccontextmanager
 from logging import getLogger
 from dotenv import load_dotenv
 from os import getenv
+from config import IS_DEV
 
 from app.oauth import oauth_router
 from app.registeration import registeration_routes
@@ -49,4 +50,4 @@ sio = AsyncServer()
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=IS_DEV)
