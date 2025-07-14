191887797794f1fa737ab8864c8599ff3666cffe
Hari21-Tech <sri21harisesh@gmail.com>
2025-07-14 22:17:58 +0530

    Test CI/CD

---
+++ tests/test_health.py
@@ -0,0 +1,11 @@
+import unittest
+from fastapi.testclient import TestClient
+from main import app  # Make sure this imports your FastAPI app
+
+client = TestClient(app)
+
+class HealthCheckTest(unittest.TestCase):
+    def test_test_endpoint(self):
+        response = client.get("/test")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "backend is up and running!"})
