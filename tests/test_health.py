490b0be3c48758f9cd171b9029478ddfc4c28349
Hari21-Tech <sri21harisesh@gmail.com>
2025-07-15 01:36:55 +0530

    PORTAL UP

--- tests/test_health.py
+++ tests/test_health.py
@@ -10,3 +10,4 @@ class HealthCheckTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "backend is up and running!"})
 #OKAY
+#OKAY2
